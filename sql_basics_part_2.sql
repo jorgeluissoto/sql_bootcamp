d49113a98f1664adcb19690a2a55f1aaf5cd0a1d
Jorge Soto <100888747+jorgeluissoto@users.noreply.github.com>
2023-03-19 17:54:23 -0400

    Create sql_basics_part_2.sql

---
+++ sql_basics_part_2.sql
@@ -0,0 +1,31 @@
+/*
+SELECT Statement
+,Distinct, Count, As, Max, Min, Avg
+*/
+
+SELECT *
+FROM EmployeeDemographics;
+
+SELECT FirstName, LastName
+FROM EmployeeDemographics;
+
+SELECT DISTINCT(EmployeeID)
+FROM EmployeeDemographics;
+
+SELECT DISTINCT(Gender)
+FROM EmployeeDemographics;
+
+SELECT COUNT(LastName) AS Last_Name_Count
+FROM EmployeeDemographics;
+
+SELECT *
+FROM EmployeeSalary;
+
+SELECT MAX(Salary)
+FROM EmployeeSalary;
+
+SELECT MIN(Salary)
+FROM EmployeeSalary;
+
+SELECT AVG(Salary)
+FROM EmployeeSalary;
