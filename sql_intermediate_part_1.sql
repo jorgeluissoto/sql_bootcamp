749708510f316f8c4744e0bfa0ae8cf7cb25ac5e
Jorge Soto <100888747+jorgeluissoto@users.noreply.github.com>
2023-03-21 21:22:32 -0400

    Create sql_intermediate_part_1.sql

---
+++ sql_intermediate_part_1.sql
@@ -0,0 +1,35 @@
+/* 
+Inner Joins, Full/ Left/ Right/ Outer Joins
+*/
+
+SELECT *
+FROM EmployeeDemographics;
+
+SELECT *
+FROM EmployeeSalary;
+
+-- Inner JOIN: Returns records that have matching values in both tables
+SELECT 
+    *
+FROM
+    EmployeeDemographics ed
+        INNER JOIN
+    EmployeeSalary es ON ed.EmployeeID = es.EmployeeID;
+
+-- FULL/ CROSS JOIN: Returns all records from both tables
+SELECT 
+    *
+FROM
+    EmployeeDemographics ed
+        CROSS JOIN
+    EmployeeSalary es ON ed.EmployeeID = es.EmployeeID;
+
+/* LEFT JOIN: Returns all records from the left table, and the matched records from the right table
+RIGHT JOIN: Returns all records from the right table, and the matched records from the left table */
+
+SELECT 
+    ed.EmployeeID, FirstName, LastName, JobTitle, Salary
+FROM
+    EmployeeDemographics ed
+        INNER JOIN
+    EmployeeSalary es ON ed.EmployeeID = es.EmployeeID;
