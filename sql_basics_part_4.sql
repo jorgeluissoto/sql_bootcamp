ed70724980b4670f7a7a630f25d5623d4435c8eb
Jorge Soto <100888747+jorgeluissoto@users.noreply.github.com>
2023-03-21 21:34:29 -0400

    Update sql_basics_part_4.sql

--- sql_basics_part_4.sql
+++ sql_basics_part_4.sql
@@ -2,31 +2,45 @@
 GROUP BY, ORDER BY
 */
 
-SELECT Gender, COUNT(Gender)
-FROM EmployeeDemographics
+SELECT 
+    Gender, COUNT(Gender)
+FROM
+    EmployeeDemographics
 GROUP BY Gender;
 
-SELECT Gender, COUNT(Gender) AS CountGender
-FROM EmployeeDemographics
-WHERE Age > 31
+SELECT 
+    Gender, COUNT(Gender) AS CountGender
+FROM
+    EmployeeDemographics
+WHERE
+    Age > 31
 GROUP BY Gender;
 
 -- ASC is the default 
-SELECT Gender, COUNT(Gender) AS CountGender
-FROM EmployeeDemographics
-WHERE Age > 31
+SELECT 
+    Gender, COUNT(Gender) AS CountGender
+FROM
+    EmployeeDemographics
+WHERE
+    Age > 31
 GROUP BY Gender
 ORDER BY CountGender DESC;
 
-SELECT *
-FROM EmployeeDemographics
+SELECT 
+    *
+FROM
+    EmployeeDemographics
 ORDER BY Age DESC;
 
-SELECT *
-FROM EmployeeDemographics
-ORDER BY Age DESC, Gender DESC;
+SELECT 
+    *
+FROM
+    EmployeeDemographics
+ORDER BY Age DESC , Gender DESC;
 
 -- number represents the column number 
-SELECT *
-FROM EmployeeDemographics
-ORDER BY 4 DESC, 5 DESC;
+SELECT 
+    *
+FROM
+    EmployeeDemographics
+ORDER BY 4 DESC , 5 DESC;
