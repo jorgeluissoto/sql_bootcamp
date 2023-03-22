183032f7b56e5563f0ab0dd899417a8c73a0a693
Jorge Soto <100888747+jorgeluissoto@users.noreply.github.com>
2023-03-21 21:33:11 -0400

    Update sql_basics_part_2.sql

--- sql_basics_part_2.sql
+++ sql_basics_part_2.sql
@@ -3,29 +3,47 @@ SELECT Statement
 ,Distinct, Count, As, Max, Min, Avg
 */
 
-SELECT *
-FROM EmployeeDemographics;
+SELECT 
+    *
+FROM
+    EmployeeDemographics;
 
-SELECT FirstName, LastName
-FROM EmployeeDemographics;
+SELECT 
+    FirstName, LastName
+FROM
+    EmployeeDemographics;
 
-SELECT DISTINCT(EmployeeID)
-FROM EmployeeDemographics;
+SELECT DISTINCT
+    (EmployeeID)
+FROM
+    EmployeeDemographics;
 
-SELECT DISTINCT(Gender)
-FROM EmployeeDemographics;
+SELECT DISTINCT
+    (Gender)
+FROM
+    EmployeeDemographics;
 
-SELECT COUNT(LastName) AS Last_Name_Count
-FROM EmployeeDemographics;
+SELECT 
+    COUNT(LastName) AS Last_Name_Count
+FROM
+    EmployeeDemographics;
 
-SELECT *
-FROM EmployeeSalary;
+SELECT 
+    *
+FROM
+    EmployeeSalary;
 
-SELECT MAX(Salary)
-FROM EmployeeSalary;
+SELECT 
+    MAX(Salary)
+FROM
+    EmployeeSalary;
 
-SELECT MIN(Salary)
-FROM EmployeeSalary;
+SELECT 
+    MIN(Salary)
+FROM
+    EmployeeSalary;
 
-SELECT AVG(Salary)
-FROM EmployeeSalary;
+SELECT 
+    AVG(Salary)
+FROM
+    EmployeeSalary;
