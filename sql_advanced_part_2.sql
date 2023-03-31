c23da7211e7fa730a7b897bfc402929fe82a8256
Jorge Soto <100888747+jorgeluissoto@users.noreply.github.com>
2023-03-31 13:25:39 -0400

    Create sql_advanced_part_2.sql

---
+++ sql_advanced_part_2.sql
@@ -0,0 +1,39 @@
+/* Temp Tables */
+
+-- Create New Temp Table 
+CREATE TEMPORARY TABLE temp_Employee(
+EmployeeID int,
+JobTitle varchar(100),
+Salary int);
+
+SELECT *
+FROM temp_Employee;
+
+-- Insert data
+INSERT INTO temp_Employee VALUES (
+'1001', 'HR', '45000' );
+
+SELECT *
+FROM temp_Employee;
+
+-- Insert data from one table into our temp table 
+INSERT INTO temp_Employee
+SELECT *
+FROM EmployeeSalary;
+
+DROP TABLE IF EXISTS temp_Employee2;
+CREATE TABLE temp_Employee2 (
+JobTitle varchar(50),
+EmployeesPerJob int,
+AvgAge int,
+AvgSalary int);
+
+INSERT INTO temp_Employee2 (
+SELECT JobTitle, Count(JobTitle), Avg(Age), AVG(salary)
+FROM EmployeeDemographics emp
+JOIN EmployeeSalary sal
+	ON emp.EmployeeID = sal.EmployeeID
+group by JobTitle);
+
+SELECT *
+FROM temp_Employee2;
